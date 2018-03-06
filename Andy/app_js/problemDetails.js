250c4ae7f18b0a8f13f5aa114e8ef45d8aa2164e
shamrock <shamrock@qq.com>
2018-03-06 17:47:15 +0800

    修改

--- Andy/app_js/problemDetails.js
+++ Andy/app_js/problemDetails.js
@@ -27,11 +27,11 @@ function replyPublic() {
         if ($.trim($fb.val()) == '') {
             console.log(1);
             $btn.addClass("fb-gray").removeClass("fb-blue").siblings("span").addClass("magn");
-            $fb.css("text-indent", "1.7rem");
+            // $fb.css("text-indent", "1.7rem");
         } else {
             $btn.addClass("fb-blue").removeClass("fb-gray").siblings("span").removeClass("magn");
             $fb.css({
-                "text-indent": "0",
+                // "text-indent": "0",
                 // "height": "1.6rem"
             });
         }
@@ -44,8 +44,7 @@ function replyPublic() {
             app = navigator.appVersion;
         var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
         if (isiOS) {
-            var $fb = $(".qa-reply .search");
-            $('textarea').focus(function() {
+            $fb.focus(function() {
                 window.setTimeout('scrollBottom()', 500);
             });
         }
@@ -102,20 +101,20 @@ function replyPublic() {
 
 }
 //�7�8����
-// function clickQuiz() {
-//     $(".qa-comment .item").each(function() {
-//         $(this).find("span.like_interest").off("touchstart");
-//         $(this).find("span.like_interest").on("touchstart", function() {
-//             if ($(this).hasClass("blue-act")) {
-//                 $(this).removeClass("blue-act");
-//                 $(this).children(".attention").removeClass("attention-blue");
-//             } else {
-//                 $(this).addClass("blue-act");
-//                 $(this).children(".attention").addClass("attention-blue");
-//             }
-//         });
-//     });
-// }
+function clickQuiz() {
+    $(".qa-comment .item").each(function() {
+        $(this).find("span.like_interest").off("touchstart");
+        $(this).find("span.like_interest").on("touchstart", function() {
+            if ($(this).hasClass("blue-act")) {
+                $(this).removeClass("blue-act");
+                $(this).children(".attention").removeClass("attention-blue");
+            } else {
+                $(this).addClass("blue-act");
+                $(this).children(".attention").addClass("attention-blue");
+            }
+        });
+    });
+}
 //����ͼ�����
 
 function ssQuiz() {
