f4135cf4bd11fa446b0c6f70f1c7bf912b0901c9
shamrock <shamrock@qq.com>
2018-03-08 16:15:45 +0800

    3.8-5

--- Andy/app_js/problemDetails.js
+++ Andy/app_js/problemDetails.js
@@ -34,68 +34,15 @@ $(function() {
     //     });
 
     // }
-
-    $('#ipt').focus(function() {
-        console.log(222)
-        var _this = $(".qa-reply .inner")
-
-        //无键盘时输入框到浏览器窗口顶部距离
-        var noInputViewHeight = $(window).height() - $(_this).height();
-
-        //网页正文内容高度
-        var contentHeight = $(document).height() - $(_this).height();
-
-        //控制正文内容高度大于一屏，保证输入框固定底部
-        contentHeight = contentHeight > noInputViewHeight ? contentHeight : noInputViewHeight;
-
-        //因为弹出输入法需要时间，需延时处理
-        setTimeout(function() {
-
-            //弹出输入法时滚动条的起始滚动距离
-            var startScrollY = $(window).scrollTop();
-
-            //弹出输入法时输入框到窗口顶部的距离，即到软键盘顶部的起始距离
-            var inputTopHeight = $(_this).offset().top - startScrollY;
-
-            //弹出输入法时输入框预期位置，即紧贴软键盘时的位置。因输入框此时处于居中状态，所以其到窗口顶部距离即为需往下移动的距离。
-            var inputTopPos = $(_this).offset().top + inputTopHeight;
-
-            //控制div不超出正文范围
-            inputTopPos = inputTopPos > contentHeight ? contentHeight : inputTopPos;
-
-            //设置输入框位置使其紧贴输入框
-            $(_this).css({ 'position': 'absolute', 'top': inputTopPos });
-
-            //给窗口对象绑定滚动事件，保证页面滚动时div能吸附软键盘
-            $(window).bind('scroll', function() {
-
-                //表示此时有软键盘存在，输入框浮在页面上了
-                if (inputTopHeight != noInputViewHeight) {
-
-                    //页面滑动后，输入框需跟随移动的距离
-                    var offset = $(this).scrollTop() - startScrollY;
-
-                    //输入框移动后位置
-                    afterScrollTopPos = inputTopPos + offset;
-
-                    //设置输入框位置使其紧贴输入框
-                    $(_this).css({ 'position': 'absolute', 'top': afterScrollTopPos });
-                }
-            });
-        }, 100);
-    }).blur(function() { //输入框失焦后还原初始状态
-        $(_this).css({ "position": "fixed", "bottom": 0 });
-        $(window).unbind('scroll');
-    });
-
+    setTimeout(function() {
+        document.body.scrollTop = document.body.scrollHeight;
+    }, 300);
 });
 
 //����ظ�����
 function replyPublic() {
     var $fb = $(".qa-reply .search");
     var $btn = $(".qa-reply .public");
-    var $searchtext = $(".qa-reply .text")
-    var timer = null;
     $fb.keyup(function() {
         if ($.trim($fb.val()) == '') {
             console.log(1);
