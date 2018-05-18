23ce9f4cdb78ddf31de2e26e5a96c39d5cf4db2b
shamrock <shamrock@qq.com>
2018-05-18 15:01:37 +0800

    改变搜索的样式

--- Andy/app_js/searchPage.js
+++ Andy/app_js/searchPage.js
@@ -1,22 +1,26 @@
 /**
  * Created by EX-ZHANGCHAOCHENG001 on 2017/12/21.
  */
-$(function(){
+$(function() {
     sp();
+    $('.sp-search .warp').on('focus', function() {
+        $(this).siblings('.btn').hide()
+    })
 });
-function sp(){
-    $(".w-list>li").each(function () {
+
+function sp() {
+    $(".w-list>li").each(function() {
         $(this).find("p").off("touchstart");
-        $(this).find("p").on("touchstart", function () {
+        $(this).find("p").on("touchstart", function() {
             if ($(this).hasClass("p-orange")) {
                 $(this).removeClass("p-orange");
-                $(".searchresult").css("display","none");
+                $(".searchresult").css("display", "none");
 
             } else {
                 //console.log("2");
                 $(".w-list>li").find("P").removeClass("p-orange");
                 $(this).addClass("p-orange");
-                $(".searchresult").css("display","block");
+                $(".searchresult").css("display", "block");
                 console.log($(this).parent().index())
             }
         });
