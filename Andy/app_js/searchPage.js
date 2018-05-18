685e76c48a38b814be48ff1356a06e22f22e9bc4
shamrock <shamrock@qq.com>
2018-05-18 16:38:48 +0800

    修改搜索样式2

--- Andy/app_js/searchPage.js
+++ Andy/app_js/searchPage.js
@@ -3,14 +3,23 @@
  */
 $(function() {
     sp();
-    $('.sp-search .warp').on('focus', function() {
-        $(this).siblings('.btn').hide();
-        $(this).css({ 'width': '18.4rem' });
-    })
-    $('.sp-search .warp').on('blur', function() {
-        $(this).siblings('.btn').show();
-        $(this).css({ 'width': '15.46666667rem' });
-    })
+    // $('.sp-search .warp').on('focus', function() {
+    //     $(this).siblings('.btn').hide();
+    //     $(this).css({ 'width': '18.4rem' });
+    // })
+    var wHeight = window.innerHeight; //获取初始可视窗口高度  
+    window.addEventListener('resize', function() { //监测窗口大小的变化事件  
+        var hh = window.innerHeight; //当前可视窗口高度  
+
+        if (wHeight > hh) { //可以作为虚拟键盘弹出事件  
+            $('.sp-search .btn').hide();
+            $('.sp-search .warp').css({ 'width': '18.4rem' });
+        } else { //可以作为虚拟键盘关闭事件  
+            $('.sp-search .btn').show();
+            $('.sp-search .warp').css({ 'width': '15.3rem' });
+        }
+        // wHeight = hh;
+    });
 });
 
 function sp() {
